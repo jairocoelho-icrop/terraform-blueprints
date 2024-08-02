a03a628dd99d8d8390fb6c63cda4cd3e9f78063c
jairo.coelho <jairo.coelho@icrop.com.br>
2024-08-02 15:45:08 -0300

    Revert fixes on replicate_source_db

--- rds/simple/main.tf
+++ rds/simple/main.tf
@@ -38,9 +38,7 @@ module "rds" {
   #db_name                = var.db_name
   username               = var.username
   port                   = var.port
-  #create_random_password = true
-  password   = var.password
-  create_random_password = var.replicate_source_db != null ? false : true
+  create_random_password = true
 
   vpc_security_group_ids = [module.security_group.security_group_id]
 
